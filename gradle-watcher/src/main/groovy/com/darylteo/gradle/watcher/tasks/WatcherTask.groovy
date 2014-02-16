d05f76232a10f0cdb554740d459d2006cc7ccbd2
darylteo <i.am@darylteo.com>
2014-02-17 00:20:55 +1100

    Make more changes to file detection mechanism

--- gradle-watcher/src/main/groovy/com/darylteo/gradle/watcher/tasks/WatcherTask.groovy
+++ gradle-watcher/src/main/groovy/com/darylteo/gradle/watcher/tasks/WatcherTask.groovy
@@ -1,15 +1,15 @@
 package com.darylteo.gradle.watcher.tasks
 
+import com.darylteo.nio.DirectoryChangedSubscriber
+import com.darylteo.nio.DirectoryWatchService
 import org.gradle.api.DefaultTask
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskAction
 import org.gradle.tooling.BuildLauncher
 import org.gradle.tooling.GradleConnector
-import org.gradle.tooling.ProgressListener
 import org.gradle.tooling.ProjectConnection
-
-import com.darylteo.nio.DirectoryChangedSubscriber
-import com.darylteo.nio.DirectoryWatchService
+import org.joda.time.Instant
+import org.joda.time.Interval
 
 class WatcherTask extends DefaultTask {
   boolean block = true
@@ -18,8 +18,16 @@ class WatcherTask extends DefaultTask {
   def includes = ['src/**']
   def excludes = []
 
+  /**
+   * how long to wait for changes to settle before triggering a build in seconds
+   */
+  def delay = 2
+
   def _timer
-  def _currentTask
+  def _lastChange
+
+  def _builder
+  def _running
 
   public WatcherTask() {
     this.group = null
@@ -47,30 +55,48 @@ class WatcherTask extends DefaultTask {
       task instanceof Task ? "$task.project.path:$task.name" : task
     } as String[]
 
-    BuildLauncher build = connection.newBuild()
+    _builder = connection.newBuild()
       .forTasks(tasks)
 
     watcher.subscribe({ Object[] args ->
       def src, path = args
-      if(_currentTask){
-        _currentTask.cancel()
-        _timer.purge()
-      }
+      _lastChange = new Instant()
 
-      _currentTask = {
-        _currentTask = null
-        build.run()
-      } as TimerTask
-
-      // Errors must be absorbed else the watcher will crap itself.
-      try {
-        _timer.schedule(_currentTask, 2000)
-      }catch(Throwable e) {
-        e.printStackTrace()
+      if(!_running) {
+        _scheduleBuild()
       }
     } as DirectoryChangedSubscriber)
 
     blockTask()
+  }
+
+  def _scheduleBuild() {
+    def currentTask = {
+      def lastKnownChange = _lastChange;
+
+      // ensure there haven't been any more recent changes
+      def elapsed = new Interval(lastKnownChange, new Instant())
+      if (elapsed.toDurationMillis() < (delay * 1000)) {
+        return
+      }
+
+      try {
+        // Errors must be absorbed else the watcher will crap itself.
+        _running = true
+        _builder.run()
+      } catch (Throwable e) {
+        e.printStackTrace()
+      } finally {
+        _running = false
+      }
+
+      // check if a change occurred during the build
+      if(!lastKnownChange.equals(_lastChange)) {
+        _scheduleBuild()
+      }
+    } as TimerTask
+
+    _timer.schedule(currentTask, delay * 1000)
   }
 
   private def blockTask() {
